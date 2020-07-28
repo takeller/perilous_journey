548e84566751904eb0f72e34ebf33f1fdc12ebc4
Taylor Keller <takeller17@gmail.com>
2020-07-28 09:27:51 -0600

    Finish iteration 1

---
+++ app/node.rb
@@ -0,0 +1,11 @@
+class Node
+
+  attr_reader :surname
+  def initialize(surname)
+    @surname = surname
+  end
+
+  def next_node
+    nil
+  end
+end
---
+++ spec/node_spec.rb
@@ -0,0 +1,21 @@
+require './app/node.rb'
+
+describe Node do
+  it 'exists' do
+    node = Node.new('Keller')
+
+    expect(node).to be_a(Node)
+  end
+
+  it '.surname' do
+    node = Node.new('Keller')
+
+    expect(node.surname).to eq('Keller')
+  end
+
+  it '.next_node' do
+    node = Node.new('Keller')
+
+    expect(node.next_node).to eq(nil)
+  end
+end
